be355c5608f844fc6d1450c8a9959522423007ac
RP082024 <161113339+RP082024@users.noreply.github.com>
2024-05-29 13:05:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(function () {
+        const image = document.getElementById('unfoldImage');
+        image.style.transform = 'scaleX(1)';
+    }, 2000); // 2000 milliseconds = 2 seconds
+};
